a07360a0d6a6327ad5b1da1b316fad8f29992d2a
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2021-03-11 17:41:51 +0900

    modelsSpecのリファクタリング

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,36 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  describe "entry with a user" do
-    before do
-      user = FactoryBot.create(:user)
-      room = Room.create
-      @entry = user.entries.create(room_id: room.id)
+  describe "entryのバリデーション" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:room) { Room.create }
+    let(:entry) { user.entries.create(room_id: room.id) }
+
+    it "有効なentry" do
+      expect(entry).to be_valid
     end
 
-    it "is valid with a user" do
-      expect(@entry).to be_valid
+    it "user_idが空白の場合無効" do
+      entry.user_id = nil
+      expect(entry).to_not be_valid
     end
 
-    it "is invalid without a user_id" do
-      @entry.user_id = nil
-      expect(@entry).to_not be_valid
+    it "room_idが空白の場合無効" do
+      entry.room_id = nil
+      expect(entry).to_not be_valid
     end
 
-    it "is invalid without a room_id" do
-      @entry.room_id = nil
-      expect(@entry).to_not be_valid
+    it "user_idとroom_idの組合わせが同じだと無刻" do
+      entry_again = Entry.new(user_id: entry.user_id, room_id: entry.room_id)
+      expect(entry_again).to_not be_valid
     end
 
-    it "is invalid with a user twice" do
-      entry2 = Entry.new(user_id: @entry.user_id, room_id: @entry.room_id)
-      expect(entry2).to_not be_valid
-    end
-
-    it "is valid entries, same room and different user" do
+    it "user_idが異なれば同じroom_idが存在しても有効" do
       other_user = FactoryBot.create(:user)
-      join_entry = Entry.new(user_id: other_user.id, room_id: @entry.room_id)
+      join_entry = Entry.new(user_id: other_user.id, room_id: entry.room_id)
       expect(join_entry).to be_valid
+    end
+
+    it "room_idが異なれば同じuser_idが存在しても有効" do
+      entry
+      other_room = Room.create
+      other_entry = Entry.new(user_id: user.id, room_id: other_room.id)
+      expect(other_entry).to be_valid
     end
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,30 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  describe "message with a user" do
-    before do
-      user = FactoryBot.create(:user)
-      room = Room.create
-      @message = user.messages.create(room_id: room.id, body: "hello")
+  describe "messageのバリデーション" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:room) { Room.create }
+    let(:message) { user.messages.create(room_id: room.id, body: "hello")}
+    
+    it "有効なメッセージ" do
+      expect(message).to be_valid
     end
 
-    it "is valid message" do
-      expect(@message).to be_valid
+    it "body属性が空白の場合無効" do
+      message.body = ""
+      expect(message).to_not be_valid
     end
 
-    it "is invalid without body" do
-      @message.body = ""
-      expect(@message).to_not be_valid
+    it "user_id属性が空白の場合無効" do
+      message.user_id = nil
+      expect(message).to_not be_valid
     end
 
-    it "is invalid without user_id" do
-      @message.user_id = nil
-      expect(@message).to_not be_valid
-    end
-
-    it "is invalid without rom_id" do
-      @message.room_id = nil
-      expect(@message).to_not be_valid
+    it "room_id属性が空白の場合無効" do
+      message.room_id = nil
+      expect(message).to_not be_valid
     end
   end
 end
