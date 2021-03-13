eab3f94d084409d915a98a4480736eaf8c24bb04
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2021-03-13 22:49:33 +0900

    database

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @users = User.where(isTalent: "true", isPublic: "true").order(created_at: "DESC").first(5)
+    @users = User.where(isTalent: true, isPublic: true).order(created_at: "DESC").first(5)
   end
 
   def new_guest
