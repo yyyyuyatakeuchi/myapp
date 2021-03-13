80bfbe2190d8141e2bec92763983ea8ac686eecc
yyyyuyatakeuchi <yuzaizhunei@gmail.com>
2021-03-13 17:34:08 +0900

    テスト（ゲスト）ログイン機能実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,16 @@ class StaticPagesController < ApplicationController
   def home
     @users = User.where(isTalent: "true", isPublic: "true").order(created_at: "DESC").first(5)
   end
+
+  def new_guest
+    email = "#{SecureRandom.base64(10)}@guest.com"
+    user = User.find_or_create_by!(name: 'GuestUser', email: email, isTalent: params[:talent]) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.confirmed_at = Time.now
+      user.created_at = Time.current.prev_year
+    end
+    user.create_talent_profile if user.isTalent
+    sign_in user
+    redirect_to mypage_path(user), notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
